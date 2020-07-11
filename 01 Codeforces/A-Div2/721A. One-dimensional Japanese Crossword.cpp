41bc251372802296b68172a69e5526e5de03e4e0
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 18:12:42 +0200

    Create 721A. One-dimensional Japanese Crossword.cpp

---
+++ 01 Codeforces/A-Div2/721A. One-dimensional Japanese Crossword.cpp
@@ -0,0 +1,125 @@
+///#pragma GCC optimize ("O3")
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+/// Info.
+///****************************************************************************************************************************************///
+/// Coder           : BAHAA El_DEEN OSAMA SAYED.
+/// Problem name    : A. One-dimensional Japanese Crossword
+/// ONline judged   : CodeForces.
+/// Sheet/Contest   : H2o
+/// Method          : implementation
+/// Date            : 4/12/2019.
+///****************************************************************************************************************************************///
+ 
+ 
+ 
+///defines
+///****************************************************************************************************************************************///
+#define rep(i,x1,x2)         for(int i=x1; i<=x2 ;++i)
+#define rep0(i,x)         for(int i=0; i<x ;i++)
+#define all(v)           ((v.begin()),((v.end())
+#define mm(v,d)          memset(v,d,sizeof(v))
+#define lp(i,v)          for(int i=0;i<sizeof(v);++i)
+#define lpi(i,j,n)       for(int i=(j);i>=(int)n;++i)
+#define lpd(i,j,n)       for(int i=(j);i>=(int)n;--i)
+#define MP               make_pair
+#define SZ(v)            ((int) ( (v).size() ) )
+#define ull              unsigned long long
+///#define ll               long long
+#define pi               3.1415926536
+#define oo               1e9
+#define OO               1e18
+#define Fx(n)            fixed<<setprecision(n)
+#define FasterIO           std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define RE0              return 0
+#define RE1              return 1
+#define mp(a,b)          make_pair(a,b)
+#define X                first
+#define Y                second
+#define pii              pair(int a, int b)
+#define rep1(i,x)        for(int i=1;i<=x;++i)
+#define RE               return
+//#define LD               long double
+///****************************************************************************************************************************************///
+ 
+/// types Defines.
+///***************************************************************************************************************************************///
+typedef long double LD;
+typedef vector<int>VI;
+typedef vector<double>VD;
+typedef vector<VD>VVD;
+typedef vector<string>VS;
+typedef pair<int ,int>PA;
+typedef pair<int,PA>PAA;
+typedef vector<int>V ;
+typedef vector<int>weight;
+typedef pair<int,int>II;
+typedef vector<vector<pair<int, int> > > VVP;
+typedef pair<int,pair<int,int> >PIP;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int, int> ii; typedef pair<int, ii> iii;
+typedef vector<int> vi;    typedef vector<ii> vii;
+ 
+///***************************************************************************************************************************************///
+ 
+ 
+///constants
+///****************************************************************************************************************************************///
+const  int  N   = (int) 1e6+10;
+const  int  M   = (int) 1e4+4;
+const  int  INF = (int) 0x3f3f3f3f;
+///const double ESP = 1e-7;
+const ll MOD=1e9+7;
+///****************************************************************************************************************************************///
+ 
+ 
+int n,res,sz(0);
+string str ;
+VI groups ;
+ 
+int main()
+{
+ 
+FasterIO;
+ 
+cin >> n >> str ;
+rep0(i,n)
+{
+    if(str[i] =='B' )
+    {
+        ++res ;
+    }
+    else if (res!=0)
+    {
+        groups.push_back(res);
+        res=0;
+    }
+}
+ 
+if(res!=0)
+{
+    groups.push_back(res);
+}
+ 
+cout << groups.size() << "\n";
+ 
+for(int a: groups)
+{
+    cout << a << " " ;
+}
+cout << "\n" ;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+RE0;}
