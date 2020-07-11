06788e9a456613a529304cce083cf69df337c0e1
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 07:46:18 +0200

    Create 746A. Compote.cpp

---
+++ 01 Codeforces/A-Div2/746A. Compote.cpp
@@ -0,0 +1,82 @@
+#pragma GCC optimize ("O3")
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+/// Info.
+///****************************************************************************************************************************************///
+/// author                : BAHAA El_DEEN OSAMA .
+/// Problem name          : A. Compote
+/// level                 : A-Div2
+/// Method                : Math
+/// Online judge          : Codeforces
+/// Date                  : 26/6/2020
+///****************************************************************************************************************************************///
+ 
+#define loop(x)          while(x--)
+#define repRot0(i,n)     for(int i=0;i*i<=n;++i)
+#define rep0(i,x)        for(ll i=0; i<x ;++i)
+#define all(v)           ((v.begin()),((v.end())
+#define mm(v,d)          memset(v,d,sizeof(v))
+#define lp(i,v)          for(int i=0;i<sizeof(v);++i)
+#define lpi(i,j,n)       for(int i=(j);i>=(int)n;++i)
+#define lpd(i,j,n)       for(int i=(j);i>=(int)n;--i)
+#define MP               make_pair
+#define SZ(v)            ((int) ( (v).size() ) )
+#define ull              unsigned long long
+#define ll               long long
+#define pi               3.1415926536
+#define oo               1e9
+#define OO               1e18
+#define Fx(n)            fixed<<setprecision(n)
+#define FasterIO           std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define RE0              return 0
+#define RE1              return 1
+#define mp(a,b)          make_pair(a,b)
+#define X                first
+#define Y                second
+#define pii              pair(int a, int b)
+#define rep1(i,x)        for(int i=1;i<x;++i)
+#define rep2(i,x)        for(int i=2;i<x;++i)
+#define rep(i,x,xx)        for(ll i=x;i<=xx;i++)
+#define RE               return
+//#define LD               long double
+#define all(v) ((v).begin()),((v).end())
+#define rev_rep(i,x)     for(int i=x ;i>0;--i)
+ 
+const  int   N  = (int) 1e6+100;
+const  int   M  = (int) 1e4+4;
+const  int  INF = (int) 0x3f3f3f3f;
+///const double ESP = 1e-7;
+///const ll MOD=1e8+9;
+ 
+ 
+int a,b,c;
+int cnt_a=0,cnt_b=0,cnt_c=0,cnt=0;
+ 
+int Solve()
+{
+       FasterIO;
+       cin>>a>>b>>c;
+       cout << 7*min(a,min(c/4,b/2));
+ 
+ 
+ 
+ 
+RE0;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+Solve();
+RE0;
+}
