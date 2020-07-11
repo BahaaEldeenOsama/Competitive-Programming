0076da53bf0914c4d61e06aafabdef69593cb607
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 22:52:09 +0200

    Create 208A. Dubstep.cpp

---
+++ 01 Codeforces/A-Div2/208A. Dubstep.cpp
@@ -0,0 +1,91 @@
+#include<fstream>
+#include<iostream>
+#include<math.h>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<math.h>
+#include<cstdio>
+#include<cctype>
+#include<cmath>
+#include<array>
+#include<assert.h>
+#include<set>
+#include<map>
+#include<valarray>
+#include<sstream>
+#include<functional>
+#include<utility>
+#include<numeric>
+#include<iterator>
+#include <bitset>
+#include <stack>
+#include <queue>
+using namespace std;
+ 
+ 
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef pair<int, int> ii;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef map<int,int> mii;
+typedef map<string,int> msi;
+typedef map<int,string> mis;
+ 
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)v.size()
+#define rsz(v) v.resize
+#define clr(v) v.clear()
+#define rev(v) reverse(all(v))
+#define lop(i,n) for(int i=0; i<n; i++)
+#define rlop(i,n) for(int i=n-1; i>=0; i--)
+#define loop(i,start,n) for(int i=start; i<n; i++)
+#define rloop(i,end,n) for(int i=n-1; i>=end; i--)
+#define PB push_back
+#define pb pop_back
+#define f first
+#define s second
+#define ord0(s) (s-'0')
+#define orda(s) (s-'a')
+#define ordA(s) (s-'A')
+ 
+int main()
+{
+ 
+   string input ;
+    int i=0 ;
+    int j=0  ;
+    cin >> input ;
+    for ( i=0 ;i<input.length();i++)
+    {
+ 
+       if(input[i]=='W'&&input[i+1]=='U'&& input[i+2]=='B')
+       {
+           i+=2;
+           cout << " " ;
+       }
+ 
+       else
+        {
+            cout << input[i] ;
+        }
+ 
+    }
+ 
+ 
+ 
+ 
+ 
+return 0 ;
+}
