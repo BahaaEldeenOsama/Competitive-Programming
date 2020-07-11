d8478e9472de6b10dbc1626580aba6c6c84ffb41
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 22:54:47 +0200

    Create 41A. Translation.cpp

---
+++ 01 Codeforces/A-Div2/41A. Translation.cpp
@@ -0,0 +1,29 @@
+#include<fstream>
+#include<iostream>
+#include<math.h>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<math.h>
+#include<cstdio>
+#include<cctype>
+#include<cmath>
+#include<array>
+#include<assert.h>
+#include<set>
+#include<map>
+using namespace std;
+int main()
+{
+string A="Null" , B="Null" ;   ///Code
+cin >>A >> B ;                /// edoc
+ 
+reverse(A.begin(),A.end());
+cout <<((A==B)?"YES":"NO") ;
+ 
+ 
+ 
+return 0 ;
+ 
+}
