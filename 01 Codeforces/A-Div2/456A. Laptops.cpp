fb4fedca4df7cd1b302d0b34ba3ac0feffbf6607
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 22:44:42 +0200

    Create 456A. Laptops.cpp

---
+++ 01 Codeforces/A-Div2/456A. Laptops.cpp
@@ -0,0 +1,38 @@
+#include<fstream>
+#include<iostream>
+#include<math.h>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<math.h>
+#include<cstdio>
+#include<cctype>
+#include<cmath>
+#include<array>
+#include<assert.h>
+#include<set>
+#include<map>
+#include<valarray>
+#include<sstream>
+#include<functional>
+#include<utility>
+#include<numeric>
+#include<iterator>
+#include<bitset>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include<vector>
+using namespace std;
+int main()
+{
+int n; cin>>n;
+ for (int i=1;i<=n;i++)
+ {int a,b; cin>>a>>b;
+  if (a!=b) {cout<<"Happy Alex"; return 0;}
+ }
+  cout<<"Poor Alex";
+return 0;
+}
+ 
