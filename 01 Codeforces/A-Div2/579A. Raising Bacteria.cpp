71645744002331d8c1b5ae55ce40f2dedf68ba7a
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 22:45:39 +0200

    Create 579A. Raising Bacteria.cpp

---
+++ 01 Codeforces/A-Div2/579A. Raising Bacteria.cpp
@@ -0,0 +1,44 @@
+#include<fstream>
+#include<iostream>
+#include<math.h>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<math.h>
+#include<cstdio>
+#include<cctype>
+#include<cmath>
+#include<array>
+#include<assert.h>
+#include<set>
+#include<map>
+#include<valarray>
+#include<sstream>
+#include<functional>
+#include<utility>
+#include<numeric>
+#include<iterator>
+#include<bitset>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include<vector>
+using namespace std;
+int main()
+{long long int a,b=0;
+	cin>>a;
+	while (a>0)
+	{
+		if (a%2==1) b++;
+		a=a/2;
+	}
+	cout<<b<<endl;
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;
+}
