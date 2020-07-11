f29c6c5aaf0793328df3a3478c747599b39ebc01
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 18:14:59 +0200

    Create 682A. Alyona and Numbers.cpp

---
+++ 01 Codeforces/A-Div2/682A. Alyona and Numbers.cpp
@@ -0,0 +1,96 @@
+///#pragma GCC optimize ("O3")
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+/// Info.
+///****************************************************************************************************************************************///
+/// I am studying Computer Science at FCICU.
+/// Coder           : BAHAA El_DEEN OSAMA SAYED.
+/// Problem name    : A. Alyona and Numbers
+/// ONline judged   : CodeForces.
+/// Assignment      : H2O
+/// Algorithm       : math
+/// Date            : 31/10/2019.
+///****************************************************************************************************************************************///
+ 
+ 
+ 
+///defines
+///****************************************************************************************************************************************///
+#define rep0(i,x)         for(int i=0; i<x ;++i)
+#define all(v)           ((v.begin()),((v.end())
+#define mm(v,d)          memset(v,d,sizeof(v))
+#define lp(i,v)          for(int i=0;i<sizeof(v);++i)
+#define lpi(i,j,n)       for(int i=(j);i>=(int)n;++i)
+#define lpd(i,j,n)       for(int i=(j);i>=(int)n;--i)
+#define MP               make_pair
+#define SZ(v)            ((int) ( (v).size() ) )
+#define ull              unsigned long long
+///#define ll               long long
+#define pi               3.1415926536
+#define oo               1e9
+#define OO               1e18
+#define Fx(n)            fixed<<setprecision(n)
+#define FasterIO           std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define RE0              return 0
+#define RE1              return 1
+#define mp(a,b)          make_pair(a,b)
+#define X                first
+#define Y                second
+#define pii              pair(int a, int b)
+#define rep1(i,x)        for(int i=1;i<=x;++i)
+#define RE               return
+//#define LD               long double
+///****************************************************************************************************************************************///
+ 
+/// types Defines.
+///***************************************************************************************************************************************///
+typedef long double LD;
+typedef vector<int>VI;
+typedef vector<double>VD;
+typedef vector<VD>VVD;
+typedef vector<string>VS;
+typedef pair<int ,int>PA;
+typedef pair<int,PA>PAA;
+typedef vector<int>V ;
+typedef vector<int>weight;
+typedef pair<int,int>II;
+typedef vector<vector<pair<int, int> > > VVP;
+typedef pair<int,pair<int,int> >PIP;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int, int> ii; typedef pair<int, ii> iii;
+typedef vector<int> vi;    typedef vector<ii> vii;
+ 
+///***************************************************************************************************************************************///
+ 
+ 
+///constants
+///****************************************************************************************************************************************///
+const  int  N   = (int) 1e6+6;
+const  int  M   = (int) 1e4+4;
+const  int  INF = (int) 0x3f3f3f3f;
+///const double ESP = 1e-7;
+const ll MOD=1e9+7;
+///****************************************************************************************************************************************///
+ 
+ 
+ll n=0 , m=0 ,cnt=0 ;
+ 
+ 
+int main()
+{
+FasterIO;
+ 
+ 
+cin >> n >> m ;
+rep1(i,n){ cnt+=(m+i%5)/5;}
+cout << cnt << endl;
+ 
+ 
+ 
+RE0;
+}
