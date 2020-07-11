74f654f8f3091a3135f1695828046b09b6a5daa8
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-12 01:45:18 +0200

    Create 304C. Lucky Permutation Triple.cpp

---
+++ 01 Codeforces/C-Div2/304C. Lucky Permutation Triple.cpp
@@ -0,0 +1,86 @@
+#include <iterator>
+#include<algorithm>
+#include<vector>
+#include<complex>
+#include<cmath>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<set>
+#include<math.h>
+#include<map>
+#include<stack>
+#include<sstream>
+#include<queue>
+#include<list>
+#include<iomanip>
+#include<bitset>
+#include<limits.h>
+#include <iostream>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<math.h>
+#include<map>
+#include<stack>
+#include<sstream>
+#include<queue>
+#include<list>
+#include<iomanip>
+#include<bitset>
+#include<limits.h>
+#include<map>
+using namespace std;
+void fast()
+{
+    ios_base ::sync_with_stdio(false) ;
+}
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+ 
+long long n=0 ; cin >> n ;
+if (n%2==0)
+{
+    cout << "-1" <<endl;
+    return 0 ;
+}
+else
+{
+ 
+for(int i=0 ; i<n ; i++)
+{
+    cout << i <<  " " ;
+}
+cout <<endl;
+for(int i=0 ; i<n ; i++)
+{
+    cout << i <<  " " ;
+}
+cout <<endl;
+ 
+for(int i=0 ; i<n ; i++)
+{
+    cout << i*2%n <<  " " ;
+}
+cout <<endl;
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+        return 0;
+}
+ 
