5e65747fcac47855c8b5b8cbac8c369ce11052aa
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 17:40:39 +0200

    Create 509A. Maximum in Table.cpp

---
+++ 01 Codeforces/A-Div2/509A. Maximum in Table.cpp
@@ -0,0 +1,55 @@
+#pragma GCC optimize ("O3")
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+/// Info.
+///****************************************************************************************************************************************///
+/// Coder               : BAHAA El_DEEN OSAMA SAYED.
+/// Problem name        : A. Maximum in Table
+/// Method              : IM
+/// Date                : 6/2/2020
+///****************************************************************************************************************************************///
+ 
+ 
+ 
+#define rep(i,x1,x2)      for(int i=x1; i<=x2 ;++i)
+#define rep0(i,x)         for(int i=0; i<x ;i++)
+#define all(v)           ((v.begin()),((v.end())
+#define mm(v,d)          memset(v,d,sizeof(v))
+#define MP               make_pair
+#define SZ(v)            ((int) ( (v).size() ) )
+#define ull              unsigned long long
+#define ll               long long
+#define pi               3.1415926536
+#define oo               1e9
+#define OO               1e18
+#define Fx(n)            fixed<<setprecision(n)
+#define FasterIO           std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define RE0              return 0
+#define RE1              return 1
+#define mp(a,b)          make_pair(a,b)
+#define X                first
+#define Y                second
+#define pii              pair(int a, int b)
+#define RE               return
+//#define LD               long double
+ 
+const  int  N   = (int) 2e5+10;
+const  int  M   = (int) 1e3+4;
+const  int  INF = (int) 0x3f3f3f3f;
+const double ESP = 1e77;
+const ll MOD=1e9+7;
+ 
+ 
+int n;
+int arr[10]= {1,2,6,20,70,252,924,3432,12870,48620};
+ 
+int main()
+{
+cin>>n;--n;cout<<arr[n];
+ 
+ 
+ 
+RE0;}
+ 
