7a15dd58bc88b220f5c0bcf3737654e41cc27138
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 22:58:31 +0200

    Create 770A. New Password.cpp

---
+++ 01 Codeforces/A-Div2/770A. New Password.cpp
@@ -0,0 +1,37 @@
+#include<fstream>
+#include<iostream>
+#include<math.h>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<math.h>
+#include<cstdio>
+#include<cctype>
+#include<cmath>
+#include<array>
+#include<assert.h>
+#include<set>
+#include<map>
+using namespace std;
+ 
+ 
+ 
+int main()
+{
+ 
+int n=0;/// all letter.
+int s=0;/// just alphabet.
+cin >> n >>s ;   ///5    1
+ 
+for(int i=0 ;i<n;i++)
+{
+    cout << char('a'+ i%s) ;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+}
