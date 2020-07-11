e9fffce28fba5d51e999cde54a266fe509be9ffb
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 22:34:45 +0200

    Create 1180A. Alex and a Rhombus.cpp

---
+++ 01 Codeforces/A-Div2/1180A. Alex and a Rhombus.cpp
@@ -0,0 +1,122 @@
+///#include<bits/stdc++.h>
+#include<iostream>
+#include<stdio.h>
+#include<memory.h>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<iterator>
+#include<algorithm>
+#include<vector>
+#include<complex>
+#include<cmath>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<set>
+#include<math.h>
+#include<map>
+#include<stack>
+#include<sstream>
+#include<queue>
+#include<list>
+#include<iomanip>
+#include<bitset>
+#include<limits.h>
+#include <iostream>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<math.h>
+#include<map>
+#include<stack>
+#include<sstream>
+#include<queue>
+#include<list>
+#include<iomanip>
+#include<bitset>
+#include<limits.h>
+#include<map>
+#include<sstream>
+#include<stdio.h>
+#include<stdio.h>
+#include<unordered_map>
+using namespace std;
+ 
+/// Info.
+///****************************************************************************************************************************************///
+/// I am studying Computer Science and artificial intelligence at Cairo University.
+/// Programmer      : BAHAA El_DEEN OSAMA SAYED.
+/// G-mail          : BEOSAMACS98@GMAIL.COM
+/// Problem name    : Alex and Rhombus.1180A
+/// ONline judged   : CodeForces
+/// Assignment      :
+/// Algorithm       : DP
+/// Date            : 20/7/2019.
+/// h:m             :
+///****************************************************************************************************************************************///
+ 
+ 
+ 
+///defines
+///****************************************************************************************************************************************///
+#define rep(i,v)         for(int i=0;i<SZ(v);++i)
+#define all(v)           ((v.begin()),((v.end())
+#define mm(v,d)          memset(v,d,sizeof(v))
+#define lp(i,v)          for(int i=0;i<sizeof(v);++i)
+#define lpi(i,j,n)       for(int i=(j);i>=(int)n;++i)
+#define lpd(i,j,n)       for(int i=(j);i>=(int)n;--i)
+#define MP               make_pair
+#define SZ(v)            ((int) ( (v).size() ) )
+#define ull              unsigned long long
+#define ll               long long
+#define pi               3.1415926536
+#define oo               1e9
+#define OO               1e19
+#define Fx(n)            fixed<<setprecision(n)
+#define Faster           std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define RE0              return 0
+#define RE1              return 1
+#define mp(a,b)          make_pair(a,b)
+#define X                first
+#define Y                second
+#define pii              pair(int a, int b)
+///****************************************************************************************************************************************///
+ 
+ 
+///***************************************************************************************************************************************///
+///constants
+///****************************************************************************************************************************************///
+const long long int  N = (int) 1e5+5;
+const long long int  M = (int) 1e4+5;
+const int  INF = (int) 0x3f3f3f3f;
+const double ESP = 1e-7;
+const int MOD=1000000007;
+///****************************************************************************************************************************************///
+ 
+int Free[100000] ;
+int solve(int n) ;
+ 
+ 
+int main()
+{
+Faster;
+int n ;cin >> n ;
+cout << solve(n) <<endl;
+ 
+ 
+RE0;}
+ 
+int solve(int n)
+{
+    if(n==0)
+        return 0 ;
+    if(n==1)
+        return 1 ;
+ 
+    return n*n + (n-1)*(n-1) ;
+}
