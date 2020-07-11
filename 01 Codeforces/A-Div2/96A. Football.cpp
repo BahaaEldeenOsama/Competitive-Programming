0a1a4d9238fb13f887b7b35738132a9ebb812ab6
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-11 18:02:37 +0200

    Create 96A. Football.cpp

---
+++ 01 Codeforces/A-Div2/96A. Football.cpp
@@ -0,0 +1,103 @@
+#pragma GCC optimize ("O3")
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+/// Info.
+///****************************************************************************************************************************************///
+/// Coder           : BAHAA El_DEEN OSAMA SAYED.
+/// Problem name    : A. Football
+/// Sheet/Contest   :
+/// Method          : String
+/// Date            : 15/12/2019.
+///****************************************************************************************************************************************///
+ 
+ 
+ 
+///defines
+///****************************************************************************************************************************************///
+#define rep(i,x1,x2)      for(int i=x1; i<=x2 ;++i)
+#define rep0(i,x)         for(int i=0; i<x ;i++)
+#define all(v)           ((v.begin()),((v.end())
+#define mm(v,d)          memset(v,d,sizeof(v))
+#define lp(i,v)          for(int i=0;i<sizeof(v);++i)
+#define lpi(i,j,n)       for(int i=(j);i>=(int)n;++i)
+#define lpd(i,j,n)       for(int i=(j);i>=(int)n;--i)
+#define MP               make_pair
+#define SZ(v)            ((int) ( (v).size() ) )
+#define ull              unsigned long long
+///#define ll               long long
+#define pi               3.1415926536
+#define oo               1e9
+#define OO               1e18
+#define Fx(n)            fixed<<setprecision(n)
+#define FasterIO           std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define RE0              return 0
+#define RE1              return 1
+#define mp(a,b)          make_pair(a,b)
+#define X                first
+#define Y                second
+#define pii              pair(int a, int b)
+#define rep1(i,x)        for(int i=1;i<=x;++i)
+#define RE               return
+//#define LD               long double
+///****************************************************************************************************************************************///
+ 
+/// types Defines.
+///***************************************************************************************************************************************///
+typedef long double LD;
+typedef vector<int>VI;
+typedef vector<double>VD;
+typedef vector<VD>VVD;
+typedef vector<string>VS;
+typedef pair<int ,int>PA;
+typedef pair<int,PA>PAA;
+typedef vector<int>V ;
+typedef vector<int>weight;
+typedef pair<int,int>II;
+typedef vector<vector<pair<int, int> > > VVP;
+typedef pair<int,pair<int,int> >PIP;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int, int> ii; typedef pair<int, ii> iii;
+typedef vector<int> vi;    typedef vector<ii> vii;
+ 
+///***************************************************************************************************************************************///
+ 
+ 
+///constants
+///****************************************************************************************************************************************///
+const  int  N   = (int) 1e6+10;
+const  int  M   = (int) 1e4+4;
+const  int  INF = (int) 0x3f3f3f3f;
+///const double ESP = 1e-7;
+const ll MOD=1e9+7;
+///****************************************************************************************************************************************///
+ 
+string str ;
+int cnt0,cnt1(0);
+ 
+int main()
+{
+FasterIO;
+ 
+cin >> str ;
+rep0(i,str.length())
+{
+    if(str[i]=='0' &&str[i+1]=='0'){++cnt0;if(cnt0==6){break;}}
+    else if(str[i]=='1' &&str[i+1]=='1'){++cnt1;if(cnt1==6){break;}}
+    else{cnt0=0,cnt1=0;}
+}
+ 
+if( cnt0>=6 || cnt1>=6 )
+{
+    cout << "YES" << "\n" ;RE0;
+}
+cout << "NO" << "\n" ;
+ 
+ 
+ 
+RE0;
+}
