c2d01d2f2529d7eb088dd09b686bea241ae2363b
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-12 08:06:15 +0200

    Create 266B. Queue at the School.cpp

---
+++ 01 Codeforces/B-Div2/266B. Queue at the School.cpp
@@ -0,0 +1,97 @@
+#pragma GCC optimize ("O3")
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+/// Info.
+///****************************************************************************************************************************************///
+/// Coder           : BAHAA El_DEEN OSAMA SAYED.
+/// Problem name    :
+/// ONline judged   : CodeForces.
+/// Sheet/Contest   : H2O
+/// Method          :
+/// Date            :31/1/2020.
+///****************************************************************************************************************************************///
+ 
+ 
+ 
+#define rep(i,x1,x2)      for(int i=x1; i<=x2 ;++i)
+#define rep0(i,x)         for(int i=0; i<x ;i++)
+#define all(v)           ((v.begin()),((v.end())
+#define mm(v,d)          memset(v,d,sizeof(v))
+#define lp(i,v)          for(int i=0;i<sizeof(v);++i)
+#define lpi(i,j,n)       for(int i=(j);i>=(int)n;++i)
+#define lpd(i,j,n)       for(int i=(j);i>=(int)n;--i)
+#define MP               make_pair
+#define SZ(v)            ((int) ( (v).size() ) )
+#define ull              unsigned long long
+///#define ll               long long
+#define pi               3.1415926536
+#define oo               1e9
+#define OO               1e18
+#define Fx(n)            fixed<<setprecision(n)
+#define FasterIO           std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define RE0              return 0
+#define RE1              return 1
+#define mp(a,b)          make_pair(a,b)
+#define X                first
+#define Y                second
+#define pii              pair(int a, int b)
+#define rep1(i,x)        for(int i=1;i<=x;++i)
+#define RE               return
+//#define LD               long double
+ 
+ 
+ 
+typedef long double LD;
+typedef vector<int>VI;
+typedef vector<double>VD;
+typedef vector<VD>VVD;
+typedef vector<string>VS;
+typedef pair<int ,int>PA;
+typedef pair<int,PA>PAA;
+typedef vector<int>V ;
+typedef vector<int>weight;
+typedef pair<int,int>II;
+typedef vector<vector<pair<int, int> > > VVP;
+typedef pair<int,pair<int,int> >PIP;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int, int> ii; typedef pair<int, ii> iii;
+typedef vector<int> vi;    typedef vector<ii> vii;
+ 
+ 
+const  int  N   = (int) 1e6+10;
+const  int  M   = (int) 1e4+4;
+const  int  INF = (int) 0x3f3f3f3f;
+///const double ESP = 1e-7;
+const ll MOD=1e9+7;
+///****************************************************************************************************************************************///
+ 
+int n,t(0);
+string str ;
+ 
+ 
+int main()
+{
+ 
+FasterIO;
+ 
+ 
+cin >> n>>t>>str;
+while(t)
+{
+    rep(i,1,str.length())
+    {
+            if(str[i]== 'G' && str[i-1]== 'B'){str[i]= 'B';str[i-1]= 'G';++i;}
+    }
+     --t;
+}
+ 
+cout << str ;
+ 
+ 
+RE0;
+}
