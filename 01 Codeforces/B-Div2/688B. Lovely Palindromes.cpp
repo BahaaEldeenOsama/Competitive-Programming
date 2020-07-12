68c84645755ec08ca87d4c246f7300fa4def99ec
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-12 08:40:10 +0200

    Create 688B. Lovely Palindromes.cpp

---
+++ 01 Codeforces/B-Div2/688B. Lovely Palindromes.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<math.h>
+#include<map>
+#include<stack>
+#include<sstream>
+#include<queue>
+#include<list>
+#include<iomanip>
+#include<bitset>
+#include<limits.h>
+#include<cmath>
+using namespace std;
+ 
+void reverseStr(string& str)
+{
+    int n = str.length();
+ 
+    // Swap character starting from two
+    // corners
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+}
+ 
+int main()
+{
+ 
+ 
+string str;cin >>str ;
+cout << str ;
+reverseStr(str);
+cout<<str ;
+ 
+ 
+ 
+ 
+return 0;
+}
