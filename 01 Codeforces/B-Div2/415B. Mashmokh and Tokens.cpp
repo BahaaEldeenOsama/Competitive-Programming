930d63a740790b99d0fae1897143b7499554f4a0
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-12 08:26:10 +0200

    Create 415B. Mashmokh and Tokens.cpp

---
+++ 01 Codeforces/B-Div2/415B. Mashmokh and Tokens.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<complex>
+#include<cmath>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<set>
+#include<math.h>
+#include<map>
+#include<stack>
+#include<sstream>
+#include<queue>
+#include<list>
+#include<iomanip>
+#include<bitset>
+#include<limits.h>
+#include <iostream>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<math.h>
+#include<map>
+#include<stack>
+#include<sstream>
+#include<queue>
+#include<list>
+#include<iomanip>
+#include<bitset>
+#include<limits.h>
+using namespace std;
+ 
+ 
+ 
+int main()
+{
+ 
+__int64	n,
+		a,
+		b,
+		t;
+	std::cin >> n >> a >> b;
+	while (n--)
+	{
+		std::cin >> t;
+		std::cout << t * a % b / a << ' ';
+	}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    return 0;
+}
+ 
