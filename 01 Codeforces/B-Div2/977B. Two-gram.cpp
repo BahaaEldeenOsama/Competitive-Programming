6ae6ff0c67b8eb14cf7551f7aaf30fa90b76792b
Bahaa El_Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-16 19:17:06 +0200

    Create 977B. Two-gram.cpp

---
+++ 01 Codeforces/B-Div2/977B. Two-gram.cpp
@@ -0,0 +1,87 @@
+#pragma GCC optimize ("O3")
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/// Info.
+///****************************************************************************************************************************************///
+/// author                : BAHAA El_DEEN OSAMA .
+/// Problem name          : B. Two-gram
+/// Method                : string
+/// Online judge          : CF
+/// Date                  : 16/7/2020
+///****************************************************************************************************************************************///
+
+
+#define ll               long long
+#define RE0              return 0
+#define loop(x)          while(x--)
+#define repRot0(i,n)     for(int i=0;i*i<=n;++i)
+#define all(v)           ((v.begin()),((v.end())
+#define mm(v,d)          memset(v,d,sizeof(v))
+#define MP               make_pair
+#define SZ(v)            ((int) ( (v).size() ) )
+#define ull              unsigned long long
+#define pi               3.1415926536
+#define oo               1e9
+#define OO               1e18
+#define Fx(n)            fixed<<setprecision(n)
+#define FasterIO           std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define RE1              return 1
+#define mp(a,b)          make_pair(a,b)
+#define X                first
+#define Y                second
+#define pii              pair(int a, int b)
+#define rep(i,x,xx)        for(ll i=x;i<xx;i++)
+#define RE               return
+#define LD               long double
+#define all(v) ((v).begin()),((v).end())
+#define rev_rep(i,x)     for(int i=x ;i>0;--i)
+
+const  int   N  = (int) 1e6+100;
+const  int   M  = (int) 1e4+4;
+const  int  INF = (int) 0x3f3f3f3f;
+const double ESP = 1e-7;
+const ll MOD=1e8+9;
+
+
+int n=0;
+string str,out;
+
+
+int Solve()
+{
+    int res=0;
+
+    cin>>n>>str;
+    rep(i,0,n-1)
+    {
+        int cur=0;
+        rep(j,0,n-1)
+        {
+            if( str[i]==str[j] && str[i+1]==str[j+1] )
+            {
+                ++cur;
+            }
+        }
+        if(res<cur)
+        {
+            res=cur;
+            out=string(1,str[i])+string(1,str[i+1]);
+        }
+    }
+    cout << out <<endl;
+
+
+ RE0;
+}
+
+
+
+
+
+int main()
+{
+Solve();
+RE0;
+}
