74b9453aaf692a2e11a3290e1e5d220af6ed42fb
Bahaa El-Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-07-20 10:36:10 +0200

    Create 682B. Alyona and Mex.cpp

---
+++ 01 Codeforces/B-Div2/682B. Alyona and Mex.cpp
@@ -0,0 +1,83 @@
+    #pragma GCC optimize ("O3")
+    #include<bits/stdc++.h>
+    using namespace std;
+
+
+
+    /// Info.
+    ///****************************************************************************************************************************************///
+    /// author                : BAHAA El_DEEN OSAMA .
+    /// Problem name          : B. Alyona and Mex
+    /// Method                : math
+    /// Online judge          : CF
+    /// Date                  : 20/7/2020
+    ///****************************************************************************************************************************************///
+
+
+    #define ll               long long
+    #define RE0              return 0
+    #define loop(x)          while(x--)
+    #define repRot0(i,n)     for(int i=0;i*i<=n;++i)
+    #define all(v)           ((v.begin()),((v.end())
+    #define mm(v,d)          memset(v,d,sizeof(v))
+    #define MP               make_pair
+    #define SZ(v)            ((int) ( (v).size() ) )
+    #define ull              unsigned long long
+    #define pi               3.1415926536
+    #define oo               1e9
+    #define OO               1e18
+    #define Fx(n)            fixed<<setprecision(n)
+    #define FasterIO           std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+    #define RE1              return 1
+    #define mp(a,b)          make_pair(a,b)
+    #define X                first
+    #define Y                second
+    #define pii              pair(int a, int b)
+    #define rep(i,x,xx)        for(int i=x;i<xx;i++)
+    #define RE               return
+    #define LD               long double
+    ///#define all(v) ((v).begin()),((v).end())
+    #define revLoop(i,x)     for(int i=x-1 ;i>=0;--i)
+/*
+    const  int   N  = (int) 1e6;
+    const  int   M  = (int) 1e4+4;
+    const  int  INF = (int) 0x3f3f3f3f;
+    const double ESP = 1e-7;
+    const ll MOD=4*1e4;
+*/
+
+    vector<int>v;
+    int n=0,val,ans=1;
+    int Solve()
+    {
+        cin>>n;
+        rep(i,0,n)
+        {
+            cin>>val;
+            v.push_back(val);
+        }
+
+        sort(v.begin(),v.end());
+
+        rep(i,0,n)
+        {
+            if(v[i]>=ans)
+            {
+                ++ans;
+            }
+        }
+        cout << ans <<endl;
+
+
+     RE0;
+    }
+
+
+
+
+
+    int main()
+    {
+    Solve();
+    RE0;
+    }
