a618c2970af0ccf6d5c829cf62f806698c172d46
Bahaa El-Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-09-07 09:53:08 +0200

    Update 506.cpp

--- 09 TopCoder/SRMs/250 DIV2/506.cpp
+++ 09 TopCoder/SRMs/250 DIV2/506.cpp
@@ -1,1 +1,48 @@
 
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+/// Info.
+///****************************************************************************************************************************************///
+/// Author                : BAHAA El_DEEN OSAMA
+/// Date                  : 6/9/2020
+///****************************************************************************************************************************************///
+
+
+class SlimeXSlimeRancher2 {
+public:
+	int train(vector <int> attributes) {
+		
+		long long res=0;
+		sort(attributes.begin(),attributes.end());
+		for(int i=0;i<attributes.size()-1;++i)
+		{
+		     res+= attributes[attributes.size()-1] - attributes[i];
+		}
+		return res;
+	}
+};
+
+
+<%:testing-code%>
+//Powered by KawigiEdit 2.1.4 (beta) modified by pivanof!
