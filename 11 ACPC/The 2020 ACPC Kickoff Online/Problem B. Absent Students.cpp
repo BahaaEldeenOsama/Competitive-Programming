56e51f79e6f6e57c966b975926f1849b1f34ab46
Bahaa El-Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-10-04 13:53:19 +0200

    Create Problem B. Absent Students.cpp

---
+++ 11 ACPC/The 2020 ACPC Kickoff Online/Problem B. Absent Students.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+long long n,m,t;
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    freopen("students.in","r",stdin);
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;
+        cout << n-m <<endl;
+    }
+
+
+
+return 0;
+}
