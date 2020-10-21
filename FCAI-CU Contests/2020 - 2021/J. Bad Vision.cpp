9c3a7393dec5ca009d9105736993c1f33d356051
Bahaa El-Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2020-10-21 12:56:16 +0200

    Create J. Bad Vision.cpp

---
+++ FCAI-CU Contests/2020 - 2021/J. Bad Vision.cpp
@@ -0,0 +1,49 @@
+#pragma GCC optimize ("O3")
+#include<iostream>
+using namespace std;
+
+
+
+/// Info.
+///****************************************************************************************************************************************///
+/// author                : BAHAA El_DEEN OSAMA .
+/// Problem name          : J. Bad Vision
+/// Method                : IM
+/// Online judge          : CF
+/// Date                  : 20/10/2020
+///****************************************************************************************************************************************///
+/*
+const  int   N  = (int) 1e6+100;
+const  int   M  = (int) 1e4+4;
+const  int  INF = (int) 0x3f3f3f3f;
+const double ESP = 1e-7;
+const ll MOD=1e8+9;
+*/
+
+int T,n;
+
+int Solve()
+{
+    freopen("vision.in","r",stdin);
+     cin>>T;
+
+     while(T--)
+     {
+        cin>>n;
+        if(n==0){cout<<8<<endl;}
+        else{cout << n <<endl;}
+     }
+
+
+return 0;
+}
+
+
+
+
+
+int main()
+{
+Solve();
+return 0;
+}
