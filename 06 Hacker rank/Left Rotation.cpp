bbb75c4400e508fcfadc7b51902b27807dadd57d
Bahaa El-Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2021-05-03 12:38:19 +0200

    Update Left Rotation.cpp

--- 06 Hacker rank/Left Rotation.cpp
+++ 06 Hacker rank/Left Rotation.cpp
@@ -14,7 +14,22 @@ int n,d;
 
 int main()
 {
+    /// O(N^2)
+    /*
+    for(int i=0;i<n;++i) cin>>arr[i];
+    for(int i=0;i<m;++i)
+    {
+        temp = arr[0];
+        for(int j=0;j<n;++j)
+        {
+            arr[j ]=arr[j+1];
+        }
+        arr[n-1] = temp;
+    }
+    for(int i=0;i<n;++i) cout <<arr[i]<< " ";
+*/
 
+    ///O(n).
     cin>>n>>d;
     for(int i=0;i<n;++i) cin>>arr[(i+n-d)%n];
     for(int i=0;i<n;++i) cout <<arr[i]<< " ";
