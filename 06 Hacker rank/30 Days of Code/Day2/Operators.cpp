247861b54394131fd061a8d4e0db63b7f8b435be
Bahaa El-Deen Osama <36152933+BahaaEldeenOsama@users.noreply.github.com>
2021-08-05 13:00:55 +0200

    Create Operators.cpp

---
+++ 06 Hacker rank/30 Days of Code/Day2/Operators.cpp
@@ -0,0 +1,26 @@
+
+/*
+ *  Hard work beats talent when talent fails to work hard.
+ *  Name   : Bahaa El-Deen osama.
+ *  Date   : 28/7/2021
+ *  Topic  : math.
+ */
+
+#define Fx            fixed<<setprecision(1)
+#define  FasterIO std::ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+
+
+int main()
+{
+    FasterIO;
+    double  a;
+    int b,c;
+    cin>>a>>b>>c;
+    cout << round( a + (a * b  /100) +  (c * b /100) ) ;
+
+
+
+
+
+    return 0;
+}
